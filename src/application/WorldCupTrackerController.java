f52c6860d77825341e1a3842db5d383dd38b762c
altaf <altaf@DESKTOP-2FG78DV.hitronhub.home>
2022-08-06 05:30:44 -0600

    Created a controller class.

---
+++ src/application/WorldCupTrackerController.java
@@ -0,0 +1,103 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class WorldCupTrackerController {
+
+    @FXML
+    private TextField countryNameTextfield;
+
+    @FXML
+    private ChoiceBox<?> resultChoicebox;
+
+    @FXML
+    private TextField playerNameTextfield;
+
+    @FXML
+    private TextField playersOfCountriesTextfield;
+
+    @FXML
+    private TextField matchesWonTextfield;
+
+    @FXML
+    private TextField ageTextfield;
+
+    @FXML
+    private TextField displayAgeTextfield;
+
+    @FXML
+    private TextField updateCountryTextfield;
+
+    @FXML
+    private TextField matchesDrawnTextfield;
+
+    @FXML
+    private Slider assistSlider;
+
+    @FXML
+    private TextField goalsScoredTextfield;
+
+    @FXML
+    private TextField updatePlayerTextfield;
+
+    @FXML
+    private TextField assistsTextfield;
+
+    @FXML
+    private Slider goalsSlider;
+
+    @FXML
+    void addCountry(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addPlayer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateCountry(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updatePlayer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewTopCountries(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewTopPlayers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewPlayersOfCountries(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewCertainAge(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewAllCountries(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewAllPlayers(ActionEvent event) {
+
+    }
+
+}
