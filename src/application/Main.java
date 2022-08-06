2ea4ff22fdc2fe9e92ad7021ce6e19b3b836e9fb
altaf <altaf@DESKTOP-2FG78DV.hitronhub.home>
2022-08-06 05:02:06 -0600

    Used scenebuilder and JavaFX to create and edit a Graphical User Interface

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,21 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("src/application/WorldCupTrackerView.fxml"));
+			Scene scene = new Scene(root,1000, 750);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Football World Cup 2022 Tracker");
 			primaryStage.show();
