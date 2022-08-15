298eb853c4341ae5ab7b6a64a80ff44e5c82395a
altaf <altaf@DESKTOP-2FG78DV.hitronhub.home>
2022-08-15 14:23:04 -0600

    Validated user inputs and added error handling and exceptions.

--- src/application/WorldCupTrackerController.java
+++ src/application/WorldCupTrackerController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class WorldCupTrackerController {
 
@@ -60,54 +61,101 @@ public class WorldCupTrackerController {
 
     @FXML
     void addCountry(ActionEvent event) {
-    	String name = countryNameTextfield.getText().toUpperCase();
-    	int matchesWon = Integer.parseInt(matchesWonTextfield.getText());
-    	int matchesDrawn = Integer.parseInt(matchesDrawnTextfield.getText());
-    	int totalGoals = Integer.parseInt(countryGoalsTextfield.getText());
-    	
-    	countryData.add(new Country(name, matchesWon, matchesDrawn, totalGoals));
-    	
-    	countryNameTextfield.clear();
-    	matchesWonTextfield.clear();
-    	matchesDrawnTextfield.clear();
+    	try {
+    		outputTextArea.clear();
+    		
+	    	String name = countryNameTextfield.getText().toUpperCase();
+	    	int matchesWon = Integer.parseInt(matchesWonTextfield.getText());
+	    	int matchesDrawn = Integer.parseInt(matchesDrawnTextfield.getText());
+	    	int totalGoals = Integer.parseInt(countryGoalsTextfield.getText());
+	    	
+	    	if (matchesWon <= 0 || matchesDrawn <= 0 || totalGoals <= 0) {
+	    		outputTextArea.setText("Matches Won, Matches Drawn and Total Goals cannot be a negative value.");
+	    	}
+	    	else {	    	
+		    	countryData.add(new Country(name, matchesWon, matchesDrawn, totalGoals));
+		    	
+		    	countryNameTextfield.clear();
+		    	matchesWonTextfield.clear();
+		    	matchesDrawnTextfield.clear();
+		    	countryGoalsTextfield.clear();
+	    	}
+    	}
+    	catch(NumberFormatException e) {
+    		outputTextArea.setText("Please use proper values for Matches Won, Matches Drawn and Total Goals.");
+    	}
+    	catch(NullPointerException e) {
+    		outputTextArea.setText("Please provide proper values.");
+    	}
     }
 
     @FXML
     void addPlayer(ActionEvent event) {
-    	String name = playerNameTextfield.getText().toUpperCase();
-    	String country = playersCountryTextfield.getText().toUpperCase();
-    	int goals = Integer.parseInt(goalsScoredTextfield.getText());
-    	
-    	playerData.add(new Player(name, country, goals));
-    	
-    	playerNameTextfield.clear();
-    	playersCountryTextfield.clear();
-    	goalsScoredTextfield.clear();
+    	try {
+	    	String name = playerNameTextfield.getText().toUpperCase();
+	    	String country = playersCountryTextfield.getText().toUpperCase();
+	    	int goals = Integer.parseInt(goalsScoredTextfield.getText());
+	    	
+	    	if (goals <= 0) {
+	    		outputTextArea.setText("Goals Scored cannot be a negative value.");
+	    	}
+		    else {	    	
+		    	playerData.add(new Player(name, country, goals));
+		    	
+		    	playerNameTextfield.clear();
+		    	playersCountryTextfield.clear();
+		    	goalsScoredTextfield.clear();
+		    }
+    	}
+    	catch(NumberFormatException e) {
+    		outputTextArea.setText("Please use proper values for Goals Scored.");
+    	}
+    	catch(NullPointerException e) {
+    		outputTextArea.setText("Please provide proper values.");
+    	}
     }
 
     @FXML
     void updateCountry(ActionEvent event) {
-    	String name = updateCountryTextfield.getText().toUpperCase();
-    	String matchResult = resultChoicebox.getValue();
-    	int goalsScored = (int) countryGoalsSlider.getValue();
-    	
-    	for (Country i: countryData) {
-    		if (i.getName().equals(name)) {
-    			i.addGoals(goalsScored);
-    			i.updateResult(matchResult);
-    		}
+    	try {
+	    	String name = updateCountryTextfield.getText().toUpperCase();
+	    	String matchResult = resultChoicebox.getValue();
+	    	int goalsScored = (int) countryGoalsSlider.getValue();
+	    	
+	    	for (Country i: countryData) {
+	    		if (i.getName().equals(name)) {
+	    			i.addGoals(goalsScored);
+	    			i.updateResult(matchResult);
+	    		}
+	    		else {
+	    			outputTextArea.setText("There is no country that matches the input");
+	    		}
+	    	}
+	    	
+	    	updateCountryTextfield.clear();
+    	}
+    	catch(NullPointerException e) {
+    		outputTextArea.setText("Please provide proper values.");
     	}
     }
 
     @FXML
     void updatePlayer(ActionEvent event) {
-    	String name = updatePlayerTextfield.getText();
-    	int goalsScored = (int) goalsSlider.getValue();   	
-    	
-    	for (Player j: playerData) {
-    		if (j.getName().equals(name)) {
-    			j.addGoals(goalsScored); 
-    		}
+    	try {
+	    	String name = updatePlayerTextfield.getText();
+	    	int goalsScored = (int) goalsSlider.getValue();   	
+	    	
+	    	for (Player j: playerData) {
+	    		if (j.getName().equals(name)) {
+	    			j.addGoals(goalsScored); 
+	    		}
+	    		else {
+	    			outputTextArea.setText("There is no player that matches the input");
+	    		}
+	    	}
+    	}
+    	catch(NullPointerException e) {
+    		outputTextArea.setText("Please provide proper values.");
     	}
     }
 
